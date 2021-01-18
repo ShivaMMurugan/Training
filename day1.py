60205386a52bbac107d1e0f2a297842e49104bd8
siva <siva.m@corestack.io>
2021-01-18 08:48:08 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,54 @@
+#Data types
+
+"""
+
+int, float, str, list, dict {"name": "arjith"}, tuple (), set ()
+boolean
+
+* variables
+
+"""
+
+
+#Int
+# a = 10
+
+# number = 10
+
+
+#float
+# ()
+# a = 1.0
+# print(type(a))
+# print(a)
+
+#strings
+
+# name = "arjith"
+# print(name)
+
+#list
+# []
+
+# fruits = ["Banana", "Apple", 'Papaya']
+
+# print(fruits)
+
+#dict
+#key value pair
+
+# names = {"name": "arjith", "age": 23, "college": "some college"}
+# print(names.get("college"))
+
+# print(names['nam'])
+
+#tuple 
+
+# t = (1, 2, 3)
+# print(t)
+
+#set
+
+#set order is not maintained, duplicated won't return
+# s = {1, 2, 3, 4, 4}
+# print(s)
