a5d44026406c9a76fd239b4f639b9310154712f8
siva <siva.m@corestack.io>
2021-01-20 09:02:31 +0530

    day 3

---
+++ day3.py
@@ -0,0 +1,60 @@
+# tree = "SOME TREE"
+# print(tree)
+
+# print(type(tree))
+
+# print(dir(tree))
+
+#upper
+
+# print(tree.upper())
+
+# print(tree.lower())
+
+# print(tree.split()) always returns a list
+
+# x = tree.split()
+# print(type(tree))
+# print(type(x))
+
+# print(tree.strip())  removes white-spaces
+
+
+# print("-".join(tree)) join a character together
+
+# txt = "I like bananas\n"
+
+# x = txt.replace("\n", "") replaces a string with a given value
+
+# print(x)
+
+
+# cap = "animal"
+
+# replica = cap.capitalize()
+# print("Printing", cap)
+# print("Printing", replica)
+
+
+#User Input
+
+
+"""
+Get input from user
+add some content
+print to the console with the given input
+"""
+
+# user_input = input("Enter a number: ") # input("")
+# print("You have entered", user_input)
+
+#only works on python 2 and below
+# user_input = input("Enter a number: ")
+
+# print("You have entered {}".format(user_input))
+
+#python 3.5 and above
+# user_input = input("Enter a number: ")
+# print(f"You have entered {user_input}")
+
+
