bd6f3c186a330fcf58d3dab03f7f1f467de4e21a
siva <siva.m@corestack.io>
2021-01-20 08:14:42 +0530

    day 2

---
+++ day2.py
@@ -0,0 +1,35 @@
+# hello = "name"
+
+# hello = "hello"
+
+# print(hello)
+
+#be careful with double quotes, single quotes
+
+#STRING METHODS
+
+# get_methods = "strings are popular"
+
+# #to get the list of methods of a variable
+
+# print(get_methods.split())
+
+
+#strip method remove whitespace
+
+# value = "some sttin "
+
+
+# print(value.strip())
+
+#format method {}place holder
+
+# age = 25
+# year = 2000
+# yr = 2500
+# print("my age is {} and {} {}".format(age, year, yr))
+
+
+#join method
+# string = "myname is something"
+# print("*".join(string))
