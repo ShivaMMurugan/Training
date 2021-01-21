ced2fca189c011040bc4d73d42f6bc071f80d0ee
siva <siva.m@corestack.io>
2021-01-21 09:02:46 +0530

    day4

---
+++ day4.py
@@ -0,0 +1,31 @@
+name = "Ananya"
+
+#len to return the length of a given item
+
+# print(len(name))
+
+# #index A N A N Y A
+#        0 1 2 3 4 5
+# print(name[::])
+# print(name[:])
+# print(name[::-1]) # reverse a string in python
+
+# if, elif , else
+
+#if 
+
+# user_input = int(input("Enter your age: "))
+
+# if user_input == 50:
+# 	print("Valid")
+# else:
+# 	print("Invalid")
+
+x = 10
+
+if not x:
+	print("Something")
+
+else:
+	print("It's valid")
+
