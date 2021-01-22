e92d3b50f9ae1277dd6dde3015593f8fe67a4131
siva <siva.m@corestack.io>
2021-01-22 08:52:31 +0530

    day5

---
+++ day5.py
@@ -0,0 +1,28 @@
+# l = [10, 20, 30, 40, 50]
+
+# print(type(l))
+# print(l)
+
+# print(l[0: 2])
+
+# Modify the list
+# print("printing line number 9", l)
+# l[2] = 100
+# print("printing line number 11", l)
+
+#creating lists in different and better way
+
+# content = list(range(1000))
+# print(content)
+
+# l1 = ["orange", 'banana', 10]
+
+# modified = l1[0]
+# x = modified.upper()
+# l1[0] = x
+# print(l1)
+# l2 = ['a', 'b', 'c']
+
+# print(l2[2])
+# l = list()
+# print(dir(l))
