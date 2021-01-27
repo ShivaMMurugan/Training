d8a634d06dbc87022f6006c1b0470d87c0e3c16d
siva <siva.m@corestack.io>
2021-01-27 08:51:41 +0530

    day6

---
+++ day6.py
@@ -0,0 +1,88 @@
+# new list
+
+l1 = [1, 2, 3, 3, 5, 5, 5, 5]
+# print(l)
+
+#append
+# word = "Animal"
+
+# print(dir(l))
+
+# l.append(word)
+# print(l)
+
+# new_list = l.append(word) This will return None
+
+#pop
+# print("line number 14", l)
+# r = l.pop()
+# print("line number 16", l)
+# print(r)
+
+# print(l.pop(3)) # returns the removed element of the index.
+# print(l)
+
+# x = l.pop() # to store the popped value of varibale we can store it in a new variable
+# print("I removed", x)
+# print(l)
+
+
+# print(l)
+
+# copy
+
+# l2 = l1.copy()
+# print("printing l1", l1)
+# print("printing l2", l2)
+
+#clear 
+# l1.clear()
+# print(l1)
+
+#count
+
+# print(l1.count(5)) count the number of same(occurences) elements in a list
+
+# l2 = ['a', 'b', 'c', 'd', 'e', 'e']
+
+
+# print(l2.count('e'))
+
+#extends and append
+# l2 = ['a', 'b', 'c', 'd', 'e', 'e']
+# l2.append(l1)
+# print("Append of l2", l2)
+# l2.extend(l1)
+# print("Extend of l2", l2)
+# print(l2)
+
+#index
+
+# print(l1.index(2)) prints the index of an element.\
+
+#sort 
+
+# l = [5, 2, 3, 4, 1]
+# l.sort()
+# print(l)
+
+#reverse
+
+# l = [1, 2, 3, 4, 5]
+# l.reverse()
+# print(l)
+
+#remove
+
+# refer difference between pop and remove
+# l = [1, 2, 3, 4, 5]
+
+# x = l.remove(4)
+# print(l)
+# print(x)
+
+#insert
+# l = [1, 2, 3, 4, 5]
+
+# l.insert(4, 6) #1st parameter index, replace value
+# print(l)
