aeb1c00e8d85d67673d7d4d4913176e574c79f30
siva <siva.m@corestack.io>
2021-01-29 08:55:29 +0530

    day8

---
+++ day8.py
@@ -0,0 +1,42 @@
+#list definition:
+# Mutuable sequence
+# l = [1, 2, 3] #original list
+# l[2] = 100 # modify the list
+# l.pop(2) # remove based on the index position of the value 
+# print(l)
+"""
+1. Mutuable sequence
+2. You can change whatever you want.
+3. Remove elements, possible
+4. You can assign new values to a list.
+5  Modify the original list
+"""
+
+#tuple defintion
+
+# t = (1, 2, 3)
+# print(dir(t))
+
+"""
+1. You cannot modify a tuple
+2. Item assignment is not possible
+3. Ordered sequence
+"""
+
+#set definition
+# myset = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+# print(myset[0]) indexing is not possible
+# myset[2] = 100 Item assignment is not possible
+# print(myset) 
+"""
+Order of insertion will not be maintained, sets will not allow duplicate
+elements.
+
+1. Item assignment is not possible 
+2. Indexing is not possible
+"""
+#Boolean values
+# a = True
+
+# if a:
+# 	print(2 * 2)
