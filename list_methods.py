eec3ca0a184d8d723232d46a5e77a7b6d250801c
siva <siva.m@corestack.io>
2021-02-04 09:02:40 +0530

    Notes

---
+++ list_methods.py
@@ -0,0 +1,36 @@
+names = ["Arvind", "Bala", "Shiva", "Dinesh"] # Mutable
+
+#Indexing []
+# print(names[0]) #valid
+# print(names[4]) #IndexError: Index out of range
+
+# Mutable
+# print("Im printing from line number 8 of list {}".format(names))
+# names[0] = "Ganesh"
+# print(f"Im printing from line number 10 of list {names}")
+
+# list methods
+
+# 1 upper only for strings
+
+# print(names.upper()) 
+# AttributeError: list object has no attribute upper
+
+# 1 append()
+
+# name = "Ganesh"
+
+# names.append(name)
+# print(names)
+
+# 2 Extend method
+
+# l = ["Ganesh", "Barath", "Surya"]
+# names.extend(l)
+# print(names)
+
+# 3 pop
+  # * removes the last element by default
+  # * can pass an index value to remove the particular field
+# names.pop()
+# print(names)
---
+++ user_input.py
@@ -0,0 +1,28 @@
+# user = input("Enter a number: ")
+# user = int(input("Enter a number: "))
+# print(user)
+# print(type(user))
+# print(user, type(user))
+
+
+#type, isinstance
+
+# user = {"name": "david", "age": 33}
+# if isinstance(user, dict):
+# 	print("It is a dict")
+# else:
+# 	print("It is not")
+
+"""
+Objective: Get a input from the user, compare your default
+value with the user's input, if user == yourvalue, then print
+valid, if not print invalid
+"""
+
+# b = 10
+# a = 10
+
+# if a == b:
+# 	print("valid")
+# else:
+# 	print("invalid")
