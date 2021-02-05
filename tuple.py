bdd83c83b3231146fe0737263e2a8ab692dac3e7
siva <siva.m@corestack.io>
2021-02-05 09:05:48 +0530

    Notes

---
+++ tuple.py
@@ -0,0 +1,29 @@
+t = (1, 2, 3, 4, 5, 5, 5, 5, 5)
+# print(t)
+# print(type(t))
+# print(dir(t))
+
+#Immutuable data type
+#Indexing is possible
+
+# print(t[0])
+
+#Cannot assign
+
+# t[0] = 10 not possible
+# print(t.count(5))
+# print(len(t))
+and, or, not, in is 
+
+if a and b:
+	print("")
+if a or b:
+	print("")
+
+a = [1, 2, 3] 
+b = [4]
+if a not in b:
+	print("")
+a == b
+
+a is b , a in b , a == b
