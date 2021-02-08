22d216e7c9abc210129e5e4a19ad44c85e62f1b1
siva <siva.m@corestack.io>
2021-02-08 09:10:58 +0530

    iterables

---
+++ iterables.py
@@ -0,0 +1,34 @@
+# What is an iterable
+
+"""
+Any object that can be loop over.
+"""
+
+# s = "sfdhfnodsghbfofgkq"
+
+# for data in s:
+# 	print(data)
+
+# l1 = [1, 2, 3, 4, 5]
+
+# for element in l1:
+# 	print(element)
+
+# number = 50 > int object is not iterable
+
+# for nums in number:
+# 	print(num)
+
+# t = (1, 2, 3, 4, 5)
+
+# for elements in t:
+# 	print(elements)
+
+# s = {1, 2, 3, 4}
+# for data in s:
+# 	print(data)
+
+# d1 = {"name": "shiva", "age": 24, "college": "DGVC"}
+
+# for key, value in d1.items():
+# 	print(key, value)
