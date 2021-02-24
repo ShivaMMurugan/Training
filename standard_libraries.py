c696be8edbbdad9a9c67f3c598c66224c96b6948
siva <siva.m@corestack.io>
2021-02-24 09:02:23 +0530

    Notes

---
+++ standard_libraries.py
@@ -0,0 +1,20 @@
+# import string
+# print(string.ascii_lowercase)
+
+# import re
+# print(dir(re))
+
+# import datetime
+
+# from string import ascii_lowercase
+# from datetime import datetime
+
+# date = datetime.utcnow()
+# print(date)
+
+# import os
+# print(os.getcwd())
+
+# my_dir = "/home/siva/Training/djsflh"
+# print(os.path.isdir(my_dir))
+
---
+++ third_party_libraries.py
@@ -0,0 +1,21 @@
+# import requests
+#pip install requests 
+
+# go = requests.get("https://google.com")
+# print(go.status_code)
+# print(go.text)
+
+# HTTP METHODS GET POST PUT DELETE
+# HTTP CODES 200, 500, 404, 201, 300
+#200-SUCCESS, 404 NOT FOUND, 500 INTERNAL SERVER ERROR
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+# if __name__ == '__main__':
+# 	app.run(debug=True)
+
